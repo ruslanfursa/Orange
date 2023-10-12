4db182267a9975a7976c608872d8ad8e3a4cb728
Ruslan Fursa <fursa_1986@mail.ru>
2023-10-12 14:57:37 +0300

    add sidebarMenu interface

--- src/test/java/model/page/LoginPage.java
+++ src/test/java/model/page/LoginPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.Set;
 
-public class LoginPage extends BasePage {
+public class LoginPage extends BasePage  {
 
     @FindBy(xpath = "//input[@name = 'username']")
     private WebElement userNameField;
